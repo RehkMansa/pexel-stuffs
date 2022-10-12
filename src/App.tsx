5696deacdba825b60dfd2fc297ca5e49a3039394
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-12 10:54:14 +0100

    added react pixelate library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-const App = () => (
-    <div className="App">
-        <h1>React TypeScript Template by rehkmansa</h1>
-    </div>
-);
+import { useState } from "react";
+import { ImagePixelated } from "react-pixelate";
+import TestImage from "./assets/rrr.png";
+import "./App.css";
+
+const App = () => {
+    const [pexels, setPexels] = useState(0);
+    return (
+        <div>
+            <ImagePixelated src={TestImage} pixelSize={pexels} centered />
+            <button onClick={() => setPexels(pexels + 5)} type="button">
+                Increase pexels
+            </button>
+        </div>
+    );
+};
 
 export default App;
